bad51f02009651e9f6d43ea21df8ab71caee0927
edinaldo10 <brasilconn@gmail.com>
2026-07-31 23:21:33 -0300

    Fix ApiTests

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -1,8 +1,9 @@
+using CloudApplication.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System.Net.Http.Json;
-using System.Net;
-using Xunit;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
 
 public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
 {
@@ -18,7 +19,7 @@ public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
         var response = await _client.GetAsync("/health");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        
+
         var json = await response.Content.ReadFromJsonAsync<Dictionary<string, string>>();
         Assert.NotNull(json);
         Assert.True(json.ContainsKey("status"));
@@ -30,7 +31,7 @@ public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
         var response = await _client.PostAsJsonAsync("/orders", new { customer = "Maria" });
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-        
+
         var order = await response.Content.ReadFromJsonAsync<Order>();
         Assert.NotNull(order);
         Assert.Equal("Maria", order.Customer);
